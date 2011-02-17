04cf25eb67a147c5e1d4d9bce57a0f082d573a95
Tanel Suurhans <tanel.suurhans@perfectline.ee>
2011-02-17 23:25:18 +0200

    - add playerlist command

---
+++ src/ee/moo/moocraft/command/PlayerListCommand.java
@@ -0,0 +1,43 @@
+package ee.moo.moocraft.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * User: Tanel Suurhans
+ * Date: 2/17/11
+ */
+public class PlayerlistCommand extends AbstractCommand {
+
+    public PlayerlistCommand(Plugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender, String command, String[] args) throws CommandException {
+
+        commandSender.sendMessage(ChatColor.GOLD + "Players online: " + this.plugin.getServer().getOnlinePlayers().length);
+
+        if (this.plugin.getServer().getOnlinePlayers().length > 0) {
+
+            for(Player player : this.plugin.getServer().getOnlinePlayers()) {
+                commandSender.sendMessage(ChatColor.GRAY + player.getDisplayName());
+            }
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isGameCommand() {
+        return true;
+    }
+
+    @Override
+    public boolean isConsoleCommand() {
+        return true;
+    }
+}
